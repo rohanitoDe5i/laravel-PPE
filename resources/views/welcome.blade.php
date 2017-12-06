49e063fad103237cd8390490a16e27f763dc4ff3
ncoudin <naotokun@hotmail.fr>
2017-12-06 18:05:36 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>M2L Parkin</title>
+        <title>M2L Parking</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
