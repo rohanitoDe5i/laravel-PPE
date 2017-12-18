9fa0d579f245ee4347f0121a6523d4d1ece176b7
rohan <rohan93x@gmail.com>
2017-12-18 11:02:51 +0100

    page acc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
           <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div style="text-align: right; font-size: large;" >
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -13,9 +13,19 @@
 
 
             <div class="content">
-                <div class="title m-b-md">
+                <div style="text-align: center; font-size: xx-large;" >
                      M2L PARKING
                 </div>
             </div>
+            <p style="text-align: center;">Afin d’éviter le stationnement sauvage dans le labyrinthe qu’est le parking, il a été décidé d’attribuer à chaque membre qui le demandait une place de parking numérotée.</br>
+            Donc nous avons décidé de mettre en place une application permettant de gérer les 
+            places.</p>
+
         </div>
+
+            <div class="content">
+                <div style="text-align: center; font-size: xx-large;" >
+                     APPLICATION SOUS DEVELOPPEMENT.
+                </div>
+            </div>
 @endsection
